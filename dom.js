042ba18b3663b11997aa1a96333ff0b416178f35
manash77 <manash.9039@gmail.com>
2023-05-23 23:28:50 +0530

    getElementByClassName completed

--- dom.js
+++ dom.js
@@ -1,2 +1,12 @@
 var headerTitle = document.getElementById('header-title');
 headerTitle.style.borderBottom = 'solid 3px #000'
+
+var Items = document.getElementsByClassName('list-group-item');
+Items[1].style.textContext = "hello 2";
+Items[1].style.fontWeight = "Bold";
+console.log(Items);
+for(var i=0;i<Items.length;i++){
+    Items[i].style.fontWeight = "Bold";
+    Items[i].style.backgroundColor = "yellow";
+}
+Items[2].style.backgroundColor = "green";
