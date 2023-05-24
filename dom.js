255b80b31e7efd6bc12d0db0c2d6a4d9bcc9bafc
manash77 <manash.9039@gmail.com>
2023-05-24 07:49:04 +0530

    task Completed

--- dom.js
+++ dom.js
@@ -1,14 +1,27 @@
-var headerTitle = document.getElementById('header-title');
-headerTitle.style.borderBottom = 'solid 3px #000'
+// var headerTitle = document.getElementById('header-title');
+// headerTitle.style.borderBottom = 'solid 3px #000'
 
-var li = document.getElementsByTagName('li');
-li[1].style.textContext = "hello 2";
-li[1].style.fontWeight = "Bold";
-console.log(li);
-for(var i=0;i<li.length;i++){
-    li[i].style.fontWeight = "Bold";
-    li[i].style.backgroundColor = "yellow";
+// var li = document.getElementsByTagName('li');
+// li[1].style.textContext = "hello 2";
+// li[1].style.fontWeight = "Bold";
+// console.log(li);
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight = "Bold";
+//     li[i].style.backgroundColor = "yellow";
+// }
+// li[2].style.backgroundColor = "green";
+// li[4].style.backgroundColor = "green";
+
+
+var li = document.querySelector('li:nth-child(2)');
+li.style.backgroundColor = 'green';
+var li = document.querySelector('li:nth-child(3)');
+li.style.display = 'none';
+
+var li = document.querySelectorAll('li');
+li[1].style.color = 'green'
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green';
 }
-li[2].style.backgroundColor = "green";
-li[4].style.backgroundColor = "green";
 
