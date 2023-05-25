a5ce892048d0992e5867d7fdded545e6837d4a54
manash77 <manash.9039@gmail.com>
2023-05-25 12:40:52 +0530

    initial commit

--- dom.js
+++ dom.js
@@ -13,15 +13,15 @@
 // li[4].style.backgroundColor = "green";
 
 
-var li = document.querySelector('li:nth-child(2)');
-li.style.backgroundColor = 'green';
-var li = document.querySelector('li:nth-child(3)');
-li.style.display = 'none';
+// var li = document.querySelector('li:nth-child(2)');
+// li.style.backgroundColor = 'green';
+// var li = document.querySelector('li:nth-child(3)');
+// li.style.display = 'none';
 
-var li = document.querySelectorAll('li');
-li[1].style.color = 'green'
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor = 'green';
-}
+// var li = document.querySelectorAll('li');
+// li[1].style.color = 'green'
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor = 'green';
+// }
 
